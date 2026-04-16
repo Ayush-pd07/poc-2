0e123f8b2050416a967b3c9ae8c8dcd598651a56
Ayush-pd07 <ayushpandey.ayush01@gmail.com>
2026-04-16 22:17:55 +0530

    Create Poc8Application.java

---
+++ src/main/java/com/example/Poc8Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Poc8Application.class, args);
+    }
+}
